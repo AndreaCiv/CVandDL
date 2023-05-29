f14143f1ef87f716895aca128eff1fd3e20cd8b4
AndreaCiv <s1092302@studenti.univpm.it>
2023-05-29 18:00:06 +0200

    aggiunta path file dataset e cartelle nel codice

--- get_images.py
+++ get_images.py
@@ -5,6 +5,7 @@ import json
 def get_data(file_name_json):
     with open(file_name_json, 'r') as file:
         json_data = json.load(file)
+        print("Numero totale foto: " + str(len(json_data)))
         file.close()
         data = []
         for index, item in enumerate(json_data):
@@ -27,7 +28,7 @@ def get_data(file_name_json):
                 data.append(bag)
             except:
                 print('********************************************')
-                print('numero '+ str(index)+ ' materiali non presenti')
+                print('numero ' + str(index) + ' materiali non presenti')
                 print('********************************************')
                 print()
                 continue
@@ -48,12 +49,37 @@ def download_images(path_dataset, data):
 
 if __name__ == "__main__":
 
+    file_name_en_cn_men = "/Users/andreacivitarese/PycharmProjects/CVandDL/dataset_non_completo_cv/en-cn/men/en-cn_men_mytheresa.json"
+    file_name_en_cn_women = "/Users/andreacivitarese/PycharmProjects/CVandDL/dataset_non_completo_cv/en-cn/women/en-cn_women_mytheresa.json"
+
     file_name_en_it_men = "/Users/andreacivitarese/PycharmProjects/CVandDL/dataset_non_completo_cv/en-it/men/en-it_men_mytheresa.json"
     file_name_en_it_women = "/Users/andreacivitarese/PycharmProjects/CVandDL/dataset_non_completo_cv/en-it/women/en-it_women_mytheresa.json"
+
+    file_name_en_jp_men = "/Users/andreacivitarese/PycharmProjects/CVandDL/dataset_non_completo_cv/en-jp/men/en-jp_men_mytheresa.json"
+    file_name_en_jp_women = "/Users/andreacivitarese/PycharmProjects/CVandDL/dataset_non_completo_cv/en-jp/women/en-jp_women_mytheresa.json"
+
+    file_name_en_kr_men = "/Users/andreacivitarese/PycharmProjects/CVandDL/dataset_non_completo_cv/en-kr/men/en-kr_men_mytheresa.json"
+    file_name_en_kr_women = "/Users/andreacivitarese/PycharmProjects/CVandDL/dataset_non_completo_cv/en-kr/women/en-kr_women_mytheresa.json"
+
+    file_name_en_us_men = "/Users/andreacivitarese/PycharmProjects/CVandDL/dataset_non_completo_cv/en-us/men/en-us_men_mytheresa.json"
+    file_name_en_us_women = "/Users/andreacivitarese/PycharmProjects/CVandDL/dataset_non_completo_cv/en-us/women/en-us_women_mytheresa.json"
+
+
+    path_dataset_en_cn_men = "/Users/andreacivitarese/PycharmProjects/CVandDL/dataset_mytheresa/en-cn/men/"
+    path_dataset_en_cn_women = "/Users/andreacivitarese/PycharmProjects/CVandDL/dataset_mytheresa/en-cn/women/"
 
     path_dataset_en_it_men = "/Users/andreacivitarese/PycharmProjects/CVandDL/dataset_mytheresa/en-it/men/"
     path_dataset_en_it_women = "/Users/andreacivitarese/PycharmProjects/CVandDL/dataset_mytheresa/en-it/women/"
 
-    data = get_data(file_name_en_it_men)
-    download_images(path_dataset_en_it_men, data)
+    path_dataset_en_jp_men = "/Users/andreacivitarese/PycharmProjects/CVandDL/dataset_mytheresa/en-jp/men/"
+    path_dataset_en_jp_women = "/Users/andreacivitarese/PycharmProjects/CVandDL/dataset_mytheresa/en-jp/women/"
+
+    path_dataset_en_kr_men = "/Users/andreacivitarese/PycharmProjects/CVandDL/dataset_mytheresa/en-kr/men/"
+    path_dataset_en_kr_women = "/Users/andreacivitarese/PycharmProjects/CVandDL/dataset_mytheresa/en-kr/women/"
+
+    path_dataset_en_us_men = "/Users/andreacivitarese/PycharmProjects/CVandDL/dataset_mytheresa/en-us/men/"
+    path_dataset_en_us_women = "/Users/andreacivitarese/PycharmProjects/CVandDL/dataset_mytheresa/en-us/women/"
+
+    data = get_data(file_name_en_us_men)
+    download_images(path_dataset_en_us_men, data)
 
